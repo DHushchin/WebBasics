0b64adc06134e475b217a0843d3eadab86ce1039
Dmytro Hushchin <dhuchchin@gmail.com>
2021-11-13 15:01:07 +0200

    Fixed configs

--- static/js/ColorChange.js
+++ static/js/ColorChange.js
@@ -1,192 +1,193 @@
-'use strict'
+"use strict";
 
 class EventEmitter {
-    constructor() {
-        this.events = {}; // hash of array of function
-    }
+  constructor() {
+    this.events = {}; // hash of array of function
+  }
 
-    on(name, fn) {
-        if (this.events[name])
-            this.events[name].push(fn);
-        else
-            this.events[name] = [fn];
+  on(name, fn) {
+    if (this.events[name]) this.events[name].push(fn);
+    else this.events[name] = [fn];
+  }
+  emit(name, ...data) {
+    if (this.events[name]) {
+      this.events[name].forEach((element) => {
+        element(...data);
+      });
     }
-    emit(name, ...data) {
-        if(this.events[name]) {
-            this.events[name].forEach(element => {
-                element(...data);
-            });
-        }
-    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    // 1 завдання - Поміняйте місцями тексти, позначені «1» та «6»
-    
-    class ChangePlaceText {
-        constructor(text1, text2) {
-            this.text1 = text1;
-            this.text2 = text2;
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  // 1 завдання - Поміняйте місцями тексти, позначені «1» та «6»
 
-        changePlace() {
-            [this.text1.innerText, this.text2.innerText] = [this.text2.innerText, this.text1.innerText]
-        }
-    } 
+  class ChangePlaceText {
+    constructor(text1, text2) {
+      this.text1 = text1;
+      this.text2 = text2;
+    }
 
-    const text1 = document.querySelector('header>p');
-    const text2 = document.querySelector('.right>p');
+    changePlace() {
+      [this.text1.innerText, this.text2.innerText] = [
+        this.text2.innerText,
+        this.text1.innerText,
+      ];
+    }
+  }
 
-    const change = new ChangePlaceText(text1, text2);
+  const text1 = document.querySelector("header>p");
+  const text2 = document.querySelector(".right>p");
 
-    change.changePlace();
+  const change = new ChangePlaceText(text1, text2);
 
-    /* 2 завдання - Напишіть функцію, яка обчислює площу кола, 
+  change.changePlace();
+
+  /* 2 завдання - Напишіть функцію, яка обчислює площу кола, 
     беручи необхідні значення із відповідних змінних у 
     скрипті, і виводить отриманий результат в кінці 
     контенту в блоці «5». */
 
-    class AreaCircle {
-        constructor(val) {
-            this.val = val;
-            this.radius = 20;
-            this.pi = Math.PI;
-        }
-
-        areaCompute() {
-            return Math.round(this.pi*Math.pow(this.radius, 2))
-        }
-
-        showResult() {
-            this.val.querySelector('p').innerText = `Radius: ${this.radius}, PI: ${this.pi}`;
-            this.val.querySelector('h3').innerText = `Answer: ${this.areaCompute()}`;
-        }
+  class AreaCircle {
+    constructor(val) {
+      this.val = val;
+      this.radius = 20;
+      this.pi = Math.PI;
     }
 
-    const circleValues = document.querySelector('.circle__info');
+    areaCompute() {
+      return Math.round(this.pi * Math.pow(this.radius, 2));
+    }
 
-    const area = new AreaCircle(circleValues);
+    showResult() {
+      this.val.querySelector(
+        "p"
+      ).innerText = `Radius: ${this.radius}, PI: ${this.pi}`;
+      this.val.querySelector("h3").innerText = `Answer: ${this.areaCompute()}`;
+    }
+  }
 
-    area.showResult();
+  const circleValues = document.querySelector(".circle__info");
 
-    /* 3 завдання - Напишіть скрипт, який знаходить мінімальну
+  const area = new AreaCircle(circleValues);
+
+  area.showResult();
+
+  /* 3 завдання - Напишіть скрипт, який знаходить мінімальну
     цифру у заданому натуральному числі, беручи це 
     число із відповідної форми в блоці «5», а отриманий 
     результат виводить за допомогою діалогового вікна 
     і зберігає в куках */
 
-class FindMinNumber {
-        constructor(num) {
-            this.num = num;
-        }
-
-        deleteCookie() {
-            document.cookie ="=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
-            location.reload()
-        }
-
-        clearForm() {
-            const html = `
-                <p>Some funny text<p>
-            `
-            document.querySelector('.left>form').innerHTML = html;
-        }
-
-        checkCookie() {
-            if(document.cookie) {
-                this.clearForm();
-
-                const saveOrNot = confirm(document.cookie + ' - save cookie?');
-
-                if(!saveOrNot) {
-                    this.deleteCookie();
-                } else {
-                    alert('Reload page, please');
-                }
-            }
-        }
-
-        findNum() {
-            const arrNum = this.num.value.split('');
-            let res = arrNum[0];
-
-            arrNum.forEach(elem => {
-                if (elem <= res) {
-                    res = elem;
-                }
-            });
-
-            document.cookie = res;
-
-            return res;
-        }
+  class FindMinNumber {
+    constructor(num) {
+      this.num = num;
     }
 
-    const num = document.querySelector('.con-form>input');
-    const subBut = document.querySelector('.con-form>button');
-    num.value = '';
+    deleteCookie() {
+      document.cookie = "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+      location.reload();
+    }
 
-    const find = new FindMinNumber(num);
+    clearForm() {
+      const html = `
+                <p>Some funny text<p>
+            `;
+      document.querySelector(".left>form").innerHTML = html;
+    }
 
-    find.checkCookie();
+    checkCookie() {
+      if (document.cookie) {
+        this.clearForm();
 
-    subBut.addEventListener('click', (event) => {
-        event.preventDefault();
-        alert(find.findNum());
-    })
+        const saveOrNot = confirm(document.cookie + " - save cookie?");
 
+        if (!saveOrNot) {
+          this.deleteCookie();
+        } else {
+          alert("Reload page, please");
+        }
+      }
+    }
 
-    /*4 завдання - Напишіть скрипт, який при настанні події select змінює колір тексту блоку «6» на 
+    findNum() {
+      const arrNum = this.num.value.split("");
+      let res = arrNum[0];
+
+      arrNum.forEach((elem) => {
+        if (elem <= res) {
+          res = elem;
+        }
+      });
+
+      document.cookie = res;
+
+      return res;
+    }
+  }
+
+  const num = document.querySelector(".con-form>input");
+  const subBut = document.querySelector(".con-form>button");
+  num.value = "";
+
+  const find = new FindMinNumber(num);
+
+  find.checkCookie();
+
+  subBut.addEventListener("click", (event) => {
+    event.preventDefault();
+    alert(find.findNum());
+  });
+
+  /*4 завдання - Напишіть скрипт, який при настанні події select змінює колір тексту блоку «6» на 
     вказаний користувачем і зберігає відповідне значення кольору в локальному 
     сховищі броузера так, щоб при наступному відкриванні сторінки значення 
     кольору тексту блоку «6» встановлювалось із збереженого значення в 
     локальному сховищі. */
 
-    class ChangeColor {
-        constructor(block) {
-            this.block = block;
-        }
-
-        saveToLocalStore(color) {
-            try {
-                localStorage.setItem('colorBlock', color);
-                return;
-            } catch(err) {
-                throw new Error(`Error save to localstorage -> ${err}`);
-            }
-        }
-        
-        change(color) {
-            this.block.style.backgroundColor = color;
-            this.saveToLocalStore(color);
-        }
+  class ChangeColor {
+    constructor(block) {
+      this.block = block;
     }
 
-    const block = document.querySelector('.right');
-    const colors = document.querySelectorAll('.right p');
+    saveToLocalStore(color) {
+      try {
+        localStorage.setItem("colorBlock", color);
+        return;
+      } catch (err) {
+        throw new Error(`Error save to localstorage -> ${err}`);
+      }
+    }
 
-    const changeCol = new ChangeColor(block);
+    change(color) {
+      this.block.style.backgroundColor = color;
+      this.saveToLocalStore(color);
+    }
+  }
 
-    const ee = new EventEmitter();
-    
-    block.style.backgroundColor = localStorage.getItem('colorBlock');
+  const block = document.querySelector(".right");
+  const colors = document.querySelectorAll(".right p");
 
-    ee.on('changeColor', (data) => {
-        changeCol.change(data);
-    });
+  const changeCol = new ChangeColor(block);
 
-    colors.forEach((elem) => {
-        try {
-            elem.addEventListener('click', function(event){
-                if(event.target.innerText === 'Light') {
-                    ee.emit('changeColor', '#E4C580');
-                }
-                else {
-                    ee.emit('changeColor', event.target.innerText);
-                }
-            });
-        } catch(err) {
-            throw new Error(err);
+  const ee = new EventEmitter();
+
+  block.style.backgroundColor = localStorage.getItem("colorBlock");
+
+  ee.on("changeColor", (data) => {
+    changeCol.change(data);
+  });
+
+  colors.forEach((elem) => {
+    try {
+      elem.addEventListener("click", function (event) {
+        if (event.target.innerText === "Light") {
+          ee.emit("changeColor", "#E4C580");
+        } else {
+          ee.emit("changeColor", event.target.innerText);
         }
-    })
-})
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
+  });
+});
--- static/js/ReadForm.js
+++ static/js/ReadForm.js
@@ -1,60 +1,60 @@
 const HttpMethods = {
-    GET: 'GET',
-    POST: 'POST',
+  GET: "GET",
+  POST: "POST",
 };
 
-const form = document.querySelector('.form');
+const form = document.querySelector(".form");
 
 const callApi = ({ data, method = HttpMethods.GET }) => {
-    return fetch(`https://dhushchin-first-website.herokuapp.com/mail`, {
-        method,
-        body: data ? JSON.stringify(data) : null,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          }
-    }).then((response) => response.json()).catch(err => console.err(`Error fetch request -> ${err}`));
+  return fetch(`https://dhushchin-first-website.herokuapp.com/mail`, {
+    method,
+    body: data ? JSON.stringify(data) : null,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .catch((err) => console.err(`Error fetch request -> ${err}`));
 };
 
 const getFormData = (form) => {
-    const formData = {};
-    new FormData(form).forEach((value, key) => {
-        formData[key] = value;
-    });
-    return formData;
+  const formData = {};
+  new FormData(form).forEach((value, key) => {
+    formData[key] = value;
+  });
+  return formData;
 };
 
 const clearFields = (form) => {
-    const fields = form.querySelectorAll('input');
-    for (const field of fields) {
-        field.value = '';
-    }
+  const fields = form.querySelectorAll("input");
+  for (const field of fields) {
+    field.value = "";
+  }
 };
 
 const sendHandler = async () => {
-    try {
-        const formData = getFormData(form);
+  try {
+    const formData = getFormData(form);
 
-        const response = await callApi({
-            data: formData,
-            method: HttpMethods.POST,
-        });
+    const response = await callApi({
+      data: formData,
+      method: HttpMethods.POST,
+    });
 
-        if (response.error) {
-            throw response;
-        } else {
-            clearFields(form);
-            alert(response);
-            return;
-        }
-    } catch(err) {
-        throw new Error(err);
+    if (response.error) {
+      throw response;
+    } else {
+      clearFields(form);
+      alert(response);
+      return;
     }
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
-
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    sendHandler();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendHandler();
 });
